85a55cfbc77118503ef2b9bce9dd6d4cda9d2e26
mbains-source <114851491+mbains-source@users.noreply.github.com>
2023-10-07 17:23:46 -0700

    exercise 5.3

--- components/Chat.js
+++ components/Chat.js
@@ -1,79 +1,60 @@
-import { useState, useEffect } from "react";
-import { StyleSheet, View, Text, KeyboardAvoidingView, Platform } from 'react-native';
-import { GiftedChat, Bubble } from "react-native-gifted-chat";
+import { useEffect, useState } from 'react';
+import { StyleSheet, View, KeyboardAvoidingView, Platform } from 'react-native';
+import { Bubble, GiftedChat } from 'react-native-gifted-chat';
+import { addDoc, collection, onSnapshot, query, orderBy } from "firebase/firestore";
 
-const Chat = ({ route, navigation }) => {
-  const { name, color } = route.params;  //Brings name and bg color selected to Chat
+const Chat = ({ route, navigation, db }) => {
+  const { name, color, background, userID } = route.params;
   const [messages, setMessages] = useState([]);
 
   const onSend = (newMessages) => {
-    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+    addDoc(collection(db, "messages"), newMessages[0]);
   }
 
   const renderBubble = (props) => {
-    return <Bubble
-      {...props}
-      wrapperStyle={{
-        right: {
-          backgroundColor: "#000"
-        },
-        left: {
-          backgroundColor: "#FFF"
-        }
-      }}
-    />
-  }
+    const wrapperStyle = background === "background1" ? {
+      right: { backgroundColor: "#000000", borderWidth: 1, borderColor: "#FFFFFF" },
+      left: { backgroundColor: "#505050", borderWidth: 1, borderColor: "#FFFFFF" }
+    } : {
+      right: { backgroundColor: "#276757", borderWidth: 1, borderColor: "#FFFFFF" },
+      left: { backgroundColor: "#50897A", borderWidth: 1, borderColor: "#FFFFFF" }
+    };
+    return <Bubble {...props} wrapperStyle={wrapperStyle} textStyle={{ right: { color: '#FFFFFF' }, left: { color: '#FFFFFF' } }} />;
+  };
 
   useEffect(() => {
     navigation.setOptions({ title: name });
-  }, []);
 
-  useEffect(() => {
-    setMessages([
-      {
-        _id: 1,
-        text: 'Hello developer',
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'React Native',
-          avatar: 'https://placeimg.com/140/140/any',
-        },
-      },
-      {
-        _id: 2,
-        text: 'This is a system message',
-        createdAt: new Date(),
-        system: true,
-      },
-    
-    ]);
+    const messagesQuery = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+    const unsubscribe = onSnapshot(messagesQuery, (snapshot) => {
+      const fetchedMessages = snapshot.docs.map(doc => {
+        const data = doc.data();
+        return {
+          ...data,
+          createdAt: data.createdAt.toDate(), // Convert Timestamp to Date
+        };
+      });
+      setMessages(fetchedMessages);
+    });
+
+    return () => unsubscribe(); // Cleanup on unmount
   }, []);
 
   return (
-    <View style={{ flex: 1, backgroundColor: color }}>
+    <View style={[styles.container, { backgroundColor: color }]}>
       <GiftedChat
         messages={messages}
         renderBubble={renderBubble}
         onSend={messages => onSend(messages)}
-        user={{
-          _id: 1
-        }}
+        user={{ _id: userID, name: name }}
       />
-    {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
-    {Platform.OS === 'ios' ? <KeyboardAvoidingView behavior="padding" /> : null }
+      {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
     </View>
   );
-  
 }
 
-    
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+  container: { flex: 1 }
 });
 
 export default Chat;
--- components/Start.js
+++ components/Start.js
@@ -1,39 +1,213 @@
-import { useState } from "react";
-import { StyleSheet, Text, TextInput, View, Button } from "react-native";
-
+import React, { useState } from "react";
+import {
+  Image,
+  TouchableOpacity,
+  ImageBackground,
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Alert,
+  Platform,
+  KeyboardAvoidingView,
+} from "react-native";
+import { getAuth, signInAnonymously } from "firebase/auth";
 
 const Start = ({ navigation }) => {
-  const [name, setName] = useState('');
+  const auth = getAuth();
+
+  const userSignIn = () => {
+    signInAnonymously(auth)
+      //get result from promise with temp user data
+      .then((result) => {
+          if (result.user.uid) {
+            navigation.navigate("Chat", { userID: result.user.uid, name: name, color: color });
+            Alert.alert("Signed in Successfully!");
+          } else {
+            Alert.alert("Something went wrong, try again!");
+          }
+        })
+        .catch((error) => {
+          Alert.alert("We could not sign you in. Try again later");
+        });
+    };
+
+  const [name, setName] = useState("");
+  const [color, setColor] = useState("#474056");
+  const [background, setBackground] = useState("background1");
+
+  const bgColors1 = {
+    black: "#000000",
+    darkGray: "#505050",
+    white: "#FFFFFF",
+    pink: "#FD77FF",
+  };
+  const bgColors2 = {
+    darkGreen: "#276757",
+    lightGreen: "#50897A",
+    yellowGreen: "#90EE90",
+    white: "#FFFFFF",
+  };
 
   return (
-    <View style={styles.container}>
-        <Text>Hello Screen1!</Text>
+    <ImageBackground
+      source={
+        background === "background1"
+          ? require("../assets/background-chat-grey.jpg")
+          : require("../assets/background-chat-green.jpg")
+      }
+      style={styles.container}
+    >
+      <View style={styles.innerContainer}>
+        <Text style={styles.title}>BlabberApp</Text>
         <TextInput
-          style={styles.nameTextInput}
-          onChangeText={setName}
+          style={styles.nameInput__input}
           value={name}
-          placeholder="Type here ..."
+          onChangeText={setName}
+          placeholder="Your username here"
+          placeholderTextColor="#757083"
+          backgroundColor="#fff"
         />
-        <Button
-          title="Go to Chat"
-          onPress={() =>
-            navigation.navigate("Chat", { name })
-          }
-        />
+        <View style={styles.backgroundSelect}>
+          <Text style={styles.backgroundSelect__text}>
+            Choose your background:
+          </Text>
+          <View style={styles.backgroundSelect__dotsWrapper}>
+            <TouchableOpacity onPress={() => setBackground("background1")}>
+              <Image
+                source={require("../assets/background-chat-grey.jpg")}
+                style={styles.backgroundSelect__dot}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => setBackground("background2")}>
+              <Image
+                source={require("../assets/background-chat-green.jpg")}
+                style={styles.backgroundSelect__dot}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={styles.colorSelect}>
+          <Text style={styles.backgroundSelect__text}>
+            Choose your background color:
+          </Text>
+          <View style={styles.colorSelect__dotsWrapper}>
+            <TouchableOpacity
+              style={[
+                styles.colorSelect__dot,
+                { backgroundColor: background === "background1" ? bgColors1.black : bgColors2.darkGreen },
+              ]}
+              onPress={() => setColor(background === "background1" ? bgColors1.black : bgColors2.darkGreen)}
+            />
+            <TouchableOpacity
+              style={[
+                styles.colorSelect__dot,
+                { backgroundColor: background === "background1" ? bgColors1.darkGray : bgColors2.lightGreen },
+              ]}
+              onPress={() => setColor(background === "background1" ? bgColors1.darkGray : bgColors2.lightGreen)}
+            />
+            <TouchableOpacity
+              style={[
+                styles.colorSelect__dot,
+                { backgroundColor: background === "background1" ? bgColors1.white : bgColors2.white },
+              ]}
+              onPress={() => setColor(background === "background1" ? bgColors1.white : bgColors2.white)}
+            />
+            <TouchableOpacity
+              style={[
+                styles.colorSelect__dot,
+                { backgroundColor: background === "background1" ? bgColors1.pink : bgColors2.yellowGreen },
+              ]}
+              onPress={() => setColor(background === "background1" ? bgColors1.pink : bgColors2.yellowGreen)}
+            />
+          </View>
+        </View>
+        <TouchableOpacity style={styles.fauxButton} onPress={userSignIn}>
+          <Text style={styles.fauxButton__text}>Start Chatting</Text>
+        </TouchableOpacity>
+        {Platform.OS === "ios" ? <KeyboardAvoidingView behavior="padding" /> : null}
       </View>
+    </ImageBackground>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center"
+    justifyContent: "flex-end",
+    alignItems: "center",
+    paddingBottom: 50,
   },
-  nameTextInput: {
+  innerContainer: {
+    width: "88%",
+    alignItems: "center",
+    backgroundColor: "rgba(255, 255, 255, 0.3)",
+    padding: 20,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 45,
+    fontWeight: "600",
+    color: "white",
+    textShadowColor: "black",
+    textShadowOffset: { width: -5, height: 5 },
+    textShadowRadius: 1,
+    marginBottom: 20,
+  },
+  backgroundSelect__text: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  nameInput__input: {
+    height: 50,
+    width: "100%",
+    borderColor: "#757083",
+    borderWidth: 1,
+    paddingLeft: 20,
+    marginBottom: 20,
+  },
+  colorSelect: {
+    alignItems: "center",
+    width: "100%",
+    marginBottom: 20,
+  },
+  colorSelect__dotsWrapper: {
+    flexDirection: "row",
+    marginTop: 10,
+  },
+  colorSelect__dot: {
+    width: 40,
     height: 40,
-    borderColor: "gray",
-    borderWidth: 1
-  }
+    borderRadius: 20,
+    marginHorizontal: 10,
+  },
+  fauxButton: {
+    backgroundColor: "#000000",
+    width: "100%",
+    padding: 15,
+    alignItems: "center",
+  },
+  fauxButton__text: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    fontWeight: "600",
+  },
+  backgroundSelect: {
+    alignItems: "center",
+    width: "100%",
+    marginBottom: 20,
+  },
+  backgroundSelect__dotsWrapper: {
+    flexDirection: "row",
+    marginTop: 10,
+  },
+  backgroundSelect__dot: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    marginHorizontal: 10,
+  },
 });
 
 export default Start;
